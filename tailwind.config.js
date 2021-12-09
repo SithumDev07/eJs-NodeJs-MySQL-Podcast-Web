687e802162c91678ab6428e28ddafcc12b392487
SithumDev07 <sithum.dashantha@gmail.com>
2021-12-10 00:27:12 +0530

    Structured the project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+		colors: {
+			'main-green' : "#17241F",
+			'secondary-yellow' : "#E8F369",
+		},
+	},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
