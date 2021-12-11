5c24b5b9e691353326f66e53b14b8c622668f110
SithumDev07 <sithum.dashantha@gmail.com>
2021-12-11 10:21:06 +0530

    Homepage done

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
-
 	res.render('index')
 })
 
+app.get('/features', (req, res) => {
+	res.render('features', { title: "Features" })
+})
+
+app.get('/pricing', (req, res) => {
+	res.render('features', { title: "Pricing" })
+})
+
+app.get('/resources', (req, res) => {
+	res.render('features', { title: "Resources" })
+})
+
+app.get('/blog', (req, res) => {
+	res.render('features', { title: "Blog" })
+})
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => console.log(`App is running on port: ${PORT}`))
